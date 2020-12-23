a21cf2e33b9850e18ed81507678868a72a9cb200
頼母木祐太 <tanomokiyuta@tanomogiyuutanoMacBook-Pro.local>
2020-12-23 15:55:22 +0900

    [Finish]3

--- app.js
+++ app.js
@@ -1,19 +1,60 @@
-var max = 100;
-var num = 1;
-var count = 0;
+// じゃんけんの手を入力してもらうプロンプト欄を生成
+var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
 
-while(num < max){
-  num = num * 2;
-  count = count + 1;
+// じゃんけんの手をランダムに作成する関数を呼び出す
+var js_hand = getJShand();
+
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し、結果をjudgeに入れる
+var judge = winLose(user_hand, js_hand);
+
+// 結果を表示する
+alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+
+// ランダムでじゃんけんの手を作成する関数
+function getJShand(){
+  var js_hand_num = Math.floor( Math.random() * 3 );
+  var hand_name;
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  } else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+
+  return hand_name;
 }
 
-alert("2をかけて" + max + "を超えるのに必要だった回数は" + count + "回です");
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
+function winLose(user, js){
+  var winLoseStr;
 
-var i;
-var num = 0;
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
 
-for(i =1; i<11; i++){
-  num =num + i;
+  return winLoseStr;
 }
-
-alert("1から10まで足算した結果は" + num + "です");
