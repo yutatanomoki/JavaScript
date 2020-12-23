b6779166245732ff3d9d47568f3d643d1b26959c
頼母木祐太 <tanomokiyuta@tanomogiyuutanoMacBook-Pro.local>
2020-12-23 14:51:56 +0900

    [Finish]2

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-var str1 = "Hello";
-var str2 = "World!!"
-alert(str1 + str2);
+var max = 100;
+var num = 1;
+var count = 0;
 
-var orange = 100;
-var apple = 120;
-
-if(orange < apple){
-  alert("みかんの値段がりんごより安い");
-} else if(orange == apple){
-  alert("みかんとリンゴが同じ値段");
-} else{
-  alert("みかんの値段がりんごより高い");
+while(num < max){
+  num = num * 2;
+  count = count + 1;
 }
 
+alert("2をかけて" + max + "を超えるのに必要だった回数は" + count + "回です");
