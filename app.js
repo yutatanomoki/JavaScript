6e78836b68557a80f97794d45449e55a61f6c9b9
頼母木祐太 <tanomokiyuta@tanomogiyuutanoMacBook-Pro.local>
2020-12-23 14:40:15 +0900

    [add]2

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-var hello = "Hello World"
-alert(hello);
+var str1 = "Hello";
+var str2 = "World!!"
+alert(str1 + str2);
+
+var orange = 100;
+var apple = 120;
+
+if(orange < apple){
+  alert("みかんの値段がりんごより安い");
+} else if(orange == apple){
+  alert("みかんとリンゴが同じ値段");
+} else{
+  alert("みかんの値段がりんごより高い");
+}
+
